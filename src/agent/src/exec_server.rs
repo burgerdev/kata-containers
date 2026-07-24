9ea82e737931925345a101db61188d4ebe1efaa2
Markus Rudy <mr@edgeless.systems>
2026-07-24 19:14:44 +0200

    fixup: test works, more refactoring

--- src/agent/src/exec_server.rs
+++ src/agent/src/exec_server.rs
@@ -6,12 +6,16 @@
 // It is only started when both `agent.debug_console` and
 // `agent.exec_noninteractive_vport` are configured.
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use async_trait::async_trait;
 use nix::sys::socket::{self, AddressFamily, SockFlag, SockType, VsockAddr};
-use protocols::exec_noninteractive::{ExecCommandRequest, ExecCommandResponse, StreamEvent};
+use protocols::exec_noninteractive::{
+    ExecCommandRequest, ExecCommandResponse, ExitStatus, StreamEvent,
+};
 use protocols::exec_noninteractive_ttrpc_async as exec_ttrpc;
 use slog::Logger;
+use std::os::fd::AsRawFd;
+use std::os::unix::process::ExitStatusExt;
 use std::process::Stdio;
 use std::sync::Arc;
 use tokio::io::{AsyncRead, AsyncReadExt, AsyncWriteExt};
@@ -34,8 +38,8 @@ pub async fn exec_noninteractive_handler(
         None,
     )?;
     let addr = VsockAddr::new(libc::VMADDR_CID_ANY, port);
-    socket::bind(listenfd, &addr)?;
-    socket::listen(listenfd, 10)?;
+    socket::bind(listenfd.as_raw_fd(), &addr)?;
+    socket::listen(&listenfd, socket::Backlog::new(10)?)?;
 
     let service = Arc::new(ExecService {
         logger: logger.clone(),
@@ -43,15 +47,14 @@ pub async fn exec_noninteractive_handler(
     let svc = exec_ttrpc::create_exec_noninteractive_service(service);
 
     let mut server = TtrpcServer::new()
-        .add_listener(listenfd)?
-        .set_domain_vsock()
+        .bind(&format!("vsock://-1:{}", port))?
         .register_service(svc);
 
     server.start().await?;
-    info!(logger, "exec noninteractive server listening"; "port" => port);
+    info!(logger, "server listening"; "port" => port);
 
     shutdown.changed().await.ok();
-    info!(logger, "exec noninteractive server shutting down");
+    info!(logger, "server shutting down");
     server.shutdown().await?;
 
     Ok(())
@@ -90,6 +93,8 @@ impl exec_ttrpc::ExecNoninteractiveService for ExecService {
             .stderr(Stdio::piped())
             .spawn()
             .map_err(|e| ttrpc::Error::Others(format!("spawn failed: {e}")))?;
+        let logger = self.logger.new(o!("pid" => child.id()));
+        info!(logger, "spawned command"; "command" => format!("{:?}", first.cmd().args()));
 
         let mut child_stdin = child.stdin.take().unwrap();
         let child_stdout = child.stdout.take().unwrap();
@@ -97,107 +102,130 @@ impl exec_ttrpc::ExecNoninteractiveService for ExecService {
 
         let tx_stdout = tx.clone();
         let tx_stderr = tx.clone();
-        let logger = self.logger.clone();
 
+        let stdin_logger = logger.new(o!("stream" => "stdin"));
         let stdin_fwd = tokio::spawn(async move {
             while let Ok(Some(req)) = rx.recv().await {
                 if !req.has_stdin() {
-                    continue;
-                }
-                let event = req.stdin();
-                if event.has_eof() && event.eof() {
+                    warn!(stdin_logger, "client sent invalid ExecCommandRequest"; "request" => format!("{}", req));
                     break;
                 }
-                if event.has_error() {
-                    // TODO(burgerdev): now what?
+                let stdin_event = req.stdin();
+                if stdin_event.has_eof() && stdin_event.eof() {
+                    info!(stdin_logger, "client sent EOF");
                     break;
                 }
-                if event.has_data() {
+                if stdin_event.has_error() {
+                    warn!(stdin_logger, "client sent an error"; "error" => stdin_event.error());
+                    break;
+                }
+                if stdin_event.has_data() {
                     // TODO(burgerdev): should this only break on EPIPE or something like that?
-                    if child_stdin.write_all(event.data()).await.is_err() {
+                    if let Err(e) = child_stdin.write_all(stdin_event.data()).await {
+                        warn!(stdin_logger, "failed writing to child stdin"; "error" => &e);
                         break;
                     }
                 }
             }
+            info!(stdin_logger, "closing");
             // EOF on the receive stream closes process stdin
             drop(child_stdin);
         });
 
+        let stdout_logger = logger.new(o!("stream" => "stdout"));
         let stdout_fwd = tokio::spawn(async move {
-            forward(child_stdout, tx_stdout, |evt| {
+            match forward(child_stdout, tx_stdout, |evt| {
                 let mut resp = ExecCommandResponse::new();
                 resp.set_stdout(evt);
                 resp
-            }).await;
+            })
+            .await
+            {
+                Err(e) => warn!(stdout_logger, "forwarding failed"; "error" => e.to_string()),
+                Ok(_) => info!(stdout_logger, "forwarding done"),
+            };
         });
 
+        let stderr_logger = logger.new(o!("stream" => "stderr"));
         let stderr_fwd = tokio::spawn(async move {
-            forward(child_stderr, tx_stderr, |evt| {
+            match forward(child_stderr, tx_stderr, |evt| {
                 let mut resp = ExecCommandResponse::new();
                 resp.set_stderr(evt);
                 resp
-            }).await;
+            })
+            .await
+            {
+                Err(e) => warn!(stderr_logger, "forwarding failed"; "error" => e.to_string()),
+                Ok(_) => info!(stderr_logger, "forwarding done"),
+            }
         });
 
+        let mut exit_status = ExitStatus::new();
+        match child.wait().await {
+            Ok(pstatus) => {
+                if let Some(signo) = pstatus.signal() {
+                    exit_status.set_signaled(signo)
+                } else if let Some(code) = pstatus.code() {
+                    exit_status.set_terminated(code)
+                } else {
+                    exit_status.set_unknown("ExitStatus had no exit code".to_owned())
+                }
+            }
+            Err(e) => {
+                warn!(logger, "waiting for child failed"; "error" => &e);
+                exit_status.set_unknown(e.to_string())
+            }
+        };
+
         let _ = tokio::join!(stdout_fwd, stderr_fwd);
-        // TODO(burgerdev): just because stdout is closed does not mean stdin is done!
-        // However, reading the docs of .wait(), it seems like stdin will be closed now anyway.
-        // We should insist on a clean stdin close before continuing!
+        // TODO(burgerdev): think about what to do with stdin.
         stdin_fwd.abort();
 
-        let exit_code = child
-            .wait()
-            .await
-            .map(|s| s.code().unwrap_or(-1)) // TODO(burgerdev): use ExitStatusExt instead of default -1.
-            .unwrap_or(-1);
-
-        info!(logger, "exec command exited"; "exit_code" => exit_code);
+        info!(logger, "exec command exited"; "exit_code" => format!("{:?}", exit_status));
 
         let mut resp = ExecCommandResponse::new();
-        resp.set_exit_code(exit_code);
-        let _ = tx.send(&resp).await;
+        resp.set_exit_status(exit_status);
+        tx.send(&resp)
+            .await
+            .map_err(|e| ttrpc::Error::Others(format!("writing final response failed: {e}")))?;
 
         Ok(())
     }
 }
 
-async fn forward<R, C>(mut r: R, tx: SSSender<ExecCommandResponse>, cons: C) -> ()
+async fn forward<R, C>(mut r: R, tx: SSSender<ExecCommandResponse>, cons: C) -> Result<()>
 where
     R: AsyncRead + Unpin,
     C: Fn(StreamEvent) -> ExecCommandResponse,
 {
     let mut buf = vec![0u8; 8192];
-    loop {
+    let mut keep_going = true;
+    while keep_going {
+        let mut event = StreamEvent::new();
         match r.read(&mut buf).await {
-            // TODO(burgerdev): inspect the error and forward to caller
             Ok(0) => {
-                let mut event = StreamEvent::new();
                 event.set_eof(true);
-                tx.send(&cons(event)).await; // TODO(burgerdev): log error
-                break;
+                keep_going = false;
             }
             Err(e) => {
-                let mut event = StreamEvent::new();
                 event.set_error(e.to_string());
-                tx.send(&cons(event)).await; // TODO(burgerdev): log error
-                break;
+                keep_going = false;
             }
             Ok(n) => {
-                let mut event = StreamEvent::new();
                 event.set_data(buf[..n].to_vec());
-                if tx.send(&cons(event)).await.is_err() {
-                    break;
-                }
             }
         }
+        tx.send(&cons(event)).await.context("sending event")?
     }
-    drop(r);
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use protocols::exec_noninteractive::ExecCommandRequest;
+    use protocols::exec_noninteractive::exit_status::Status;
+    use protocols::exec_noninteractive::stream_event::Event;
+    use protocols::exec_noninteractive::{exec_command_response::Output, ExecCommandRequest};
     use protocols::exec_noninteractive_ttrpc_async::ExecNoninteractiveServiceClient;
     use ttrpc::r#async::Server as TtrpcServer;
 
@@ -216,10 +244,8 @@ mod tests {
     }
 
     // Run a command through the exec service and collect all output.
-    // Calls close_send() after the initial request so commands that read
-    // stdin (e.g. cat) receive EOF and exit cleanly.
     async fn run(addr: &str, cmd: Vec<&str>, stdin: &[u8]) -> (Vec<u8>, Vec<u8>, i32) {
-        let client = ttrpc::r#async::Client::connect(addr).unwrap();
+        let client = ttrpc::r#async::Client::connect(addr).await.unwrap();
         let svc = ExecNoninteractiveServiceClient::new(client);
         let bidi = svc
             .run_command(ttrpc::context::with_timeout(0))
@@ -227,36 +253,56 @@ mod tests {
             .unwrap();
         let (tx, mut rx) = bidi.split();
 
+        // Send command.
         let mut req = ExecCommandRequest::new();
         req.mut_cmd()
             .set_args(cmd.iter().map(|s| s.to_string()).collect());
-        // TODO(burgerdev): do we need stdin here?
-        // req.stdin = stdin.to_vec();
         tx.send(&req).await.unwrap();
-        // Signal EOF on the client send direction so stdin-consuming
-        // commands know there is no more input.  Ignore errors here:
-        // if the server closed the stream early the send side may
-        // already be gone.
-        let _ = tx.close_send().await;
+
+        // Send stdin.
+        let mut req = ExecCommandRequest::new();
+        req.mut_stdin().set_data(stdin.to_vec());
+        tx.send(&req).await.unwrap();
+
+        // Close stdin.
+        let mut req = ExecCommandRequest::new();
+        req.mut_stdin().set_eof(true);
+        tx.send(&req).await.unwrap();
 
         let mut stdout_data = Vec::new();
         let mut stderr_data = Vec::new();
-        let mut exit_code = -1i32;
         loop {
             match rx.recv().await {
-                Ok(resp) => match resp.payload {
-                    Some(Payload::Stdout(d)) => stdout_data.extend_from_slice(&d),
-                    Some(Payload::Stderr(d)) => stderr_data.extend_from_slice(&d),
-                    Some(Payload::ExitCode(c)) => {
-                        exit_code = c;
-                        break;
+                Ok(resp) => match resp.output {
+                    Some(Output::Stdout(d)) => handle_stream_event(&mut stdout_data, &d),
+                    Some(Output::Stderr(d)) => handle_stream_event(&mut stderr_data, &d),
+                    Some(Output::ExitStatus(status)) => {
+                        let code = match status.status.unwrap() {
+                            Status::Unknown(msg) => {
+                                panic!("server reported unknown status: {}", msg)
+                            }
+                            Status::Signaled(signo) => 128 + signo,
+                            Status::Terminated(code) => code,
+                            s => panic!("server sent unsupported status message: {:?}", s),
+                        };
+                        return (stdout_data, stderr_data, code);
                     }
-                    None => {}
+                    _ => {}
                 },
-                Err(_) => break,
+                Err(e) => panic!("recv error: {}", e),
             }
         }
-        (stdout_data, stderr_data, exit_code)
+    }
+
+    fn handle_stream_event(collector: &mut Vec<u8>, evt: &StreamEvent) {
+        if let Some(e) = &evt.event {
+            match e {
+                Event::Data(d) => collector.extend_from_slice(&d),
+                Event::Eof(_) => (),
+                Event::Error(err) => panic!("{}", err),
+                _ => panic!("unexpected event type: {:?}", e),
+            }
+        }
     }
 
     #[tokio::test]
@@ -280,6 +326,18 @@ mod tests {
         let (_stdout, _stderr, code) = run(addr, vec!["sh", "-c", "exit 42"], &[]).await;
 
         assert_eq!(code, 42);
+
+        server.shutdown().await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn test_signal() {
+        let addr = "unix://@/tmp/kata-exec-test-signal";
+        let mut server = start_test_server(addr).await;
+
+        let (_stdout, _stderr, code) = run(addr, vec!["sh", "-c", "kill $$"], &[]).await;
+
+        assert_eq!(code, 143);
 
         server.shutdown().await.unwrap();
     }
@@ -318,7 +376,7 @@ mod tests {
         // A nonexistent command causes spawn() to fail.  The service returns a
         // ttrpc error rather than panicking, and the client sees the stream
         // close with an error on recv().
-        let client = ttrpc::r#async::Client::connect(addr).unwrap();
+        let client = ttrpc::r#async::Client::connect(addr).await.unwrap();
         let svc = ExecNoninteractiveServiceClient::new(client);
         let bidi = svc
             .run_command(ttrpc::context::with_timeout(0))
